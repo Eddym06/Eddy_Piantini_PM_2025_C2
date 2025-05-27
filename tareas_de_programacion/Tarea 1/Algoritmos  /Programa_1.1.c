df0e0d2d75358684c1e409911681167d6e88ecef
Eddy <eddym062806@gmail.com>
2025-05-27 09:14:39 -0400

    Renombrar carpetas y archivos para evitar errores

---
+++ tareas_de_programacion/Tarea 1/Algoritmos  /Programa_1.1.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+
+/* Programa 1.1
+El siguiente es el primer programa escrito en el lenguaje C. */
+
+void main (void) {
+    printf("Mi primer programa en C");
+}
+
+/*Este programa lo que hace es mostrar en la consola el texto: Mi primer programa en C. */
